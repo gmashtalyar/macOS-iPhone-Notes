51ef5a5f5e0f89e4f8a0fb0c52f77573b58023dc
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-13 16:31:12 +0300

    Initial Commit

---
+++ MacOS iPhone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacOS iPhone
+//
+//  Created by Геннадий Машталяр on 13.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacOS iPhone/MacOS_iPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_iPhoneApp.swift
+//  MacOS iPhone
+//
+//  Created by Геннадий Машталяр on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_iPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
